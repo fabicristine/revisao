29760fc61ef0a083bf584fd59dd4d5cba17c3d00
fabicristine <fabi.cristine@hotmail.com>
2015-03-22 20:43:31 -0300

    Segundo commit

--- src/main/java/ucdb/br/servlets/CalculadoraServlet.java
+++ src/main/java/ucdb/br/servlets/CalculadoraServlet.java
@@ -22,15 +22,19 @@ public class CalculadoraServlet extends HttpServlet{
 		
 		String n1= req.getParameter("n1");
 		String n2= req.getParameter("n2");
+		String op= req.getParameter("op");
 	
 		int num1= Integer.parseInt(n1);
 		int num2= Integer.parseInt(n2);
+		int operacao= Integer.parseInt(op);
 		
 		Calculadora calc= new Calculadora();
-		int s= calc.somar(num1,num2);
+		
+		
+		int res= calc.calcular(num1,num2,operacao);
 		
 		PrintWriter pw= resp.getWriter();
-		pw.print("A soma é: "+s);
+		pw.print("O resultado é: "+res);
 		
 		//PrintWriter pw= resp.getWriter();
 		//pw.print("Você digitou n1: "+n1 + " n2: "+n2);
--- src/main/java/ucdb/br/util/Calculadora.java
+++ src/main/java/ucdb/br/util/Calculadora.java
@@ -6,4 +6,29 @@ public class Calculadora {
 		return num1 + num2;
 	}
 
+	public int subtrair(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public int multiplicar(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int dividir(int num1, int num2) {
+		return num1/num2;
+	}
+
+	public int calcular(int num1, int num2, int operacao) {
+		
+		if(operacao==1){
+			return somar(num1,num2);
+		}else if(operacao==2){
+			return subtrair(num1,num2);
+		}else if(operacao==3){
+			return multiplicar(num1,num2);
+		}else{
+			return dividir(num1,num2);
+		}
+	}
+
 }
